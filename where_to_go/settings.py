e79a4247da5ee184475d3dd1b2c1dd268a381f9d
Anton Pozdnyakov <anton.pozdnyakov.i@gmail.com>
2020-06-22 20:42:35 +0300

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+print('STATIC_URL', STATIC_URL)
+print('STATICFILES_DIRS', STATICFILES_DIRS)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def show_mainpage(request):
-    return render(request, 'main.html')
+    return render(request, 'index.html')

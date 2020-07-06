fb4629b4bfc10f761bfb6f4e996a55f899cbf84f
Anton Pozdnyakov <anton.pozdnyakov.i@gmail.com>
2020-07-06 14:33:49 +0300

    Add WYSIWYG for description_long

--- places/models.py
+++ places/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField("Название", max_length=50)
     description_short = models.TextField("Короткое описание",)
-    description_long = models.TextField("Полное описание",)
+    description_long = HTMLField("Полное описание",)
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from where_to_go import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_mainpage),
     path('places/<int:id>/', views.get_location),
+    path('tinymce/', include('tinymce.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

7319816c9c4fb1f0e35fb27af09b856ff8601e40
Anton Pozdnyakov <anton.pozdnyakov.i@gmail.com>
2020-07-23 10:19:53 +0300

    Update `static_url`

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = os.getenv("STATIC_URL")
+#STATIC_URL = os.getenv("STATIC_URL")
+STATIC_URL="/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_dir"),

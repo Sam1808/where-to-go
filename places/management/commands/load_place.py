8c8643b829b89fe0c163011da4e66e76f64f3599
Anton Pozdnyakov <anton.pozdnyakov.i@gmail.com>
2020-09-03 20:31:53 +0300

    Check HTTP status (step# 37)

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,8 +1,8 @@
 import requests
 from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from places.models import Place, Image
+from requests.exceptions import ConnectionError, HTTPError
 
 class Command(BaseCommand):
     help = 'Add location info to database.'
@@ -11,10 +11,29 @@ class Command(BaseCommand):
         parser.add_argument('location_data')
 
     def handle(self, *args, **options):
-
         json_url = options['location_data']
-        response = requests.get(json_url)
+
+        try: #test connection
+            response = requests.get(json_url)
+            response.raise_for_status()
+        except ConnectionError:
+            print('Connection Error')
+            exit()
+        
+        headers = {'Accept': 'application/json'}
+        response_status = requests.get(  
+            f'https://httpstat.us/{response.status_code}', headers=headers).json() # get description for status code
+       
+
+        if response_status['code'] != 200:
+            print( f"Error:{response_status['description']}")
+            exit()
+        
+
         json_data = response.json()
+
+        if 'error' in json_data: # for crooked sites with errors and code 200
+            raise requests.exceptions.HTTPError(json_data['error'])
 
         location, created = Place.objects.get_or_create(
             title=json_data['title'],
@@ -28,6 +47,14 @@ class Command(BaseCommand):
             for image_url in image_urls_catalog:
                 image_name = image_url.split('/')[-1]
                 response = requests.get(image_url)
+                
+                response_status= requests.get(
+                    f'https://httpstat.us/{response.status_code}', headers=headers).json()
+
+                if response_status['code'] != 200:
+                    print(f"Error:{response_status['description']}")
+                    # exit() Maybe I will be lucky with the next picture...
+
                 picture, created = Image.objects.get_or_create(
                     image=image_name,
                     place=location,
